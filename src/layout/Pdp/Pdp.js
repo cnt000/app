56855f5f483a05ee79f068fd5601e21106c87e9f
Edoardo Gargano <cnt000@gmail.com>
2020-05-30 15:50:02 +0200

    pdp compo

--- src/layout/Pdp/Pdp.js
+++ src/layout/Pdp/Pdp.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import styles from './Pdp.module.css';
 
-const Pdp = ({ productName, productImage, vaseDiameter, price, addToCart}) => (
+const Pdp = ({ productName, productImage, vaseDiameter, price, addToCart }) => (
   <main classNameName={styles.pdpContent} role="main">
     <div className={styles.header}></div>
     <div className={styles.mainImage}>
-      <img
-        src="http://www.pungilandia.com/php/images/hi/adromischus-clavifolius.jpg"
-        alt="Adromischus  clavifolius"
-      />
+      <img src={productImage} alt={productName} />
     </div>
     <div className={styles.productInfo}>
-      <h2 className={styles.name}>Adromischus clavifolius</h2>
-      <div className={styles.dimension}>Vaso 24cm</div>
-      <div className={styles.price}>Prezzo: € 9,00</div>
+      <h2 className={styles.name}>{productName}</h2>
+      <div className={styles.dimension}>{vaseDiameter}</div>
+      <div className={styles.price}>{price}</div>
       <button className={styles.addtoCart}>{addToCart}</button>
     </div>
   </main>
--- src/layout/Pdp/Pdp.stories.js
+++ src/layout/Pdp/Pdp.stories.js
@@ -3,9 +3,17 @@ import React from 'react';
 import Pdp from './Pdp';
 import labels from '../../labels/labels.json';
 
+const productInfo = {
+  productName: 'Adromischus clavifolius',
+  productImage:
+    'http://www.pungilandia.com/php/images/hi/adromischus-clavifolius.jpg',
+  vaseDiameter: 'Vaso 24cm',
+  price: 'Prezzo: € 9,00',
+};
+
 export default {
   component: Pdp,
   title: 'Pdp',
 };
 
-export const Default = () => <Pdp {...labels} />;
+export const Default = () => <Pdp {...labels} {...productInfo} />;
