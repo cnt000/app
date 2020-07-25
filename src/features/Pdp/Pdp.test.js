c46136d1ef7367bc1c1ad3f0826dfb8b6db7be73
Edoardo Gargano <edoardo.gargano@monrif.net>
2020-07-25 09:54:34 +0200

    fix build

--- src/features/Pdp/Pdp.test.js
+++ src/features/Pdp/Pdp.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import Pdp from './Pdp';
-import LabelsContext from '../labels';
+import LabelsContext from '../Labels';
 import labels from '../../data/labels/labels.json';
 import product from '../../data/products/clavifolius.json';
 
