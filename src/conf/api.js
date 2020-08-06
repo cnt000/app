46693ce818051807c88b8a1ab0fc1facf9d45c65
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2020-08-06 19:18:46 +0200

    env var fixed

--- src/conf/api.js
+++ src/conf/api.js
@@ -1,5 +1,5 @@
 export const conf = {
   apiEndpoint:
-    process.env.REACT_APP_API_ENDPOINT || 'https://pungilandia2020.appspot.com',
+    process.env.REACT_APP_API_DOMAIN || 'https://pungilandia2020.appspot.com',
   plpUrl: process.env.REACT_APP_API_PLP_BASEURL || '/api/v1/search/',
 };
