ba3655aea03a5c4d2b843225b0eae1f43c6f4bf9
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2020-10-11 22:22:51 +0200

    fix api url

--- src/conf/url.js
+++ src/conf/url.js
@@ -1,7 +1,7 @@
 export const conf = {
   apiEndpoint:
     process.env.REACT_APP_API_DOMAIN ||
-    'https://pungilandia2020-app.appspot.com',
+    'https://pungilandia2020-api.appspot.com',
   plpUrl: process.env.REACT_APP_API_PLP_BASEURL || '/api/v1/search/',
   pdpUrl: process.env.REACT_APP_API_PDP_BASEURL || '/api/v1/item/',
   responsiveImageService: `https://qp2dljrm.twic.pics`
