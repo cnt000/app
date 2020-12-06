b80764efdbc7205e15b0763ed0c2580070638c4a
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2020-12-06 10:34:36 +0100

    fix test

--- src/features/Plp/Pagination/Pagination.test.js
+++ src/features/Plp/Pagination/Pagination.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Pagination from './Pagination';
-import LabelsContext from '../Labels';
-import labels from '../../data/labels/labels.json';
+import LabelsContext from '../../Labels';
+import labels from '../../../data/labels/labels.json';
 
 it('renders correctly', () => {
   const tree = renderer
